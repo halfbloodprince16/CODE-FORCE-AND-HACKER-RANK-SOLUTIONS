f28265ee73356eee763c9aa5d0098861025d307e
Half Blood Prince <26187718+halfbloodprince16@users.noreply.github.com>
2018-06-05 11:55:08 +0530

    Add files via upload
    hacker rank problem

--- test.cpp
+++ test.cpp
@@ -1,47 +1,21 @@
-void findSmallest(int m, int s)
-{
-    if (s == 0)
-    {
-        (m == 1)? cout << "Smallest number is " << 0
-                : cout << "Not possible";
-        return ;
-    }
-
-    if (s > 9*m)
-    {
-        cout << "Not possible";
-        return ;
-    }
- 
-    int res[m];
-
-    s -= 1;
-
-    for (int i=m-1; i>0; i--)
-    {
-
-        if (s > 9)
-        {
-            res[i] = 9;
-            s -= 9;
-        }
-        else
-        {
-            res[i] = s;
-            s = 0;
-        }
-    }
-
-    res[0] = s + 1;
- 
-    cout << "Smallest number is ";
-    for (int i=0; i<m; i++)
-        cout << res[i];
-}
+#include"bits/stdc++.h"
+using namespace std;
 
 int main()
 {
-    int s = 10;
-    findSmallest(m, s);
-    return 0;
+    int n,i;
+    cin>>n;
+    int x[n],hash[n]={};
+    for(i=0;i<n;i++)
+    {
+        cin>>x[i];
+        hash[x[i]]++;
+    }
+    for(i=1;i<=n;i++)
+    {
+        if(hash[i] == 0)
+        {
+            cout<<i<<" ";
+        }
+    }
 }
