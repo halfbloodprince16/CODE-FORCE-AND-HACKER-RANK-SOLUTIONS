e19bc111d52eabd302ce1309620b87d5c66858d4
ilikegirls <26187718+halfbloodprince16@users.noreply.github.com>
2018-12-18 23:08:54 +0530

    Add files via upload

--- HR_MinSwaps.cpp
+++ HR_MinSwaps.cpp
@@ -1,33 +1,55 @@
-#include"bits/stdc++.h"
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<vector>
+#include<stack>
+#include<set>
+#include<queue>
+#include<map>
+#include<math.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
-typedef long long int ll;
-vector<int>v;
-vector<int>u;
-priority_queue<int>pq;
-std::map<int,int> m;
-const int N=1e6+10;
-/*--------------------------hbp16@hbp16-Inspiron-3543:-$------------------------------*/
-int main(int argc, char const *argv[])
+#define FOR(i,a,b) for(i=a;i<=b;i++)
+#define sint(i) scanf("%d",&i)
+#define ss(s) scanf("%s",s)
+#define pii pair<int,int>
+#define mp(i,j) make_pair(i,j)
+#define ll long long
+#define MAX 1000000000
+#define MOD 1000000007
+#define vi vector<int>
+#define vvi vector < vi >
+#define pb(i) push_back(i);
+#define tr(v,it) for(it=v.begin();it!=v.end();it++)
+#define DEBUG 0
+int main()
 {
-	fast;
-	ll n,k,i,j;
-	string s;
-	int cnt=0;
-	ll hash[101]={};
-	
-	cin>>n;
-	int x[n];
-	for(i=0;i<n;i++)
-	{
-		cin>>x[i];
-		if(x[i] != i+1)
-		{
-			cnt++;
-		}
-	}
+	#if DEBUG
+	freopen("test.in", "r", stdin);
+    freopen("test.out", "w", stdout);
+    #endif
+    int t,test;
+    int i;
+    
+	    int n;
+	    cin>>n;
+	    int a[n+1];
+	    FOR(i,1,n) {
+	    	cin>>a[i];
+	    }
+	    i=1;
+	    int c=0;
+	    while(i<=n) {
+	    	while(a[i]!=i)
+	    	{
+	    		swap(a[a[i]],a[i]);
+	    		c++;
+	    	}
+	    	i++;
+	    }
+	    cout<<c;
 
-	cout<<cnt-1;
 
 	return 0;
 }
