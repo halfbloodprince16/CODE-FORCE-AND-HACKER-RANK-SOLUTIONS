e821f0716d369faf7753014427d95f08fa2aaafb
Vighnesh Tiwari <vighneshtiwari16377@gmail.com>
2020-02-26 15:32:42 +0530

    Add files via upload

---
+++ 1283A.cpp
@@ -0,0 +1,29 @@
+#include"bits/stdc++.h" 
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+typedef long long int ll;
+vector<int>v;
+vector<int>u;
+vector<int>::iterator itr;
+priority_queue<int>pq;
+const int N=1e6+10;
+/*--------------------------hbp16@hbp16-Inspiron-3543:-$------------------------------*/
+void solve()
+{
+	int a,b;
+	int x=23,y=60;
+	cin>>a>>b;
+	cout<<((x-a)*60+(y-b))<<endl;
+}
+
+int main(int argc, char const *argv[])
+{
+	int t;
+	cin>>t;
+
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
