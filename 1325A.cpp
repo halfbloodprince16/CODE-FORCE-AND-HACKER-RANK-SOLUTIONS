9722d457e96662d3e5347e26eb7e5db2198c82f4
Vighnesh Tiwari <vighneshtiwari16377@gmail.com>
2020-03-14 21:35:07 +0530

    Add files via upload

---
+++ 1325A.cpp
@@ -0,0 +1,15 @@
+#include"bits/stdc++.h" 
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+typedef long long int ll;
+vector<int>u;
+vector<int>w;
+vector<int>::iterator it;
+priority_queue<int>pq;
+const int N=1e5+10;
+/*--------------------------hbp16@hbp16-Inspiron-3543:-$------------------------------*/
+int main(int argc, char const *argv[])
+{
+	cout<<std::lcm(10,12);
+	return 0;
+}
